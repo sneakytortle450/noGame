e266678d1476d5f48669c413e799d6f7158ae409
Turk Smith <turksmith2008@gmail.com>
2026-05-02 11:46:24 -0700

    inital commit

---
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -0,0 +1,92 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private Stage stage;
+    private Skin skin;
+
+    @Override
+    public void create() {
+        stage = new Stage(new FitViewport(640, 480));
+        skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+
+        Window window = new Window("Example screen", skin, "border");
+        window.defaults().pad(4f);
+        window.add("This is a simple Scene2D view.").row();
+        final TextButton button = new TextButton("Click me!", skin);
+        button.pad(8f);
+        button.addListener(new ChangeListener() {
+            @Override
+            public void changed(final ChangeEvent event, final Actor actor) {
+                button.setText("Clicked.");
+            }
+        });
+        window.add(button);
+        window.pack();
+        // We round the window position to avoid awkward half-pixel artifacts.
+        // Casting using (int) would also work.
+        window.setPosition(MathUtils.roundPositive(stage.getWidth() / 2f - window.getWidth() / 2f),
+            MathUtils.roundPositive(stage.getHeight() / 2f - window.getHeight() / 2f));
+        window.addAction(Actions.sequence(Actions.alpha(0f), Actions.fadeIn(1f)));
+        stage.addActor(window);
+
+        Window window2 = new Window("Example screen", skin, "border");
+        window2.defaults().pad(4f);
+        window2.add("This is a simple Scene2D view.").row();
+        final TextButton button2 = new TextButton("Click me!", skin);
+        button2.pad(8f);
+        button2.addListener(new ChangeListener() {
+            @Override
+            public void changed(final ChangeEvent event, final Actor actor) {
+                button2.setText("Clicked.");
+
+                window.remove();
+            }
+        });
+        window2.add(button2);
+        window2.pack();
+        // We round the window position to avoid awkward half-pixel artifacts.
+        // Casting using (int) would also work.
+        window2.setPosition(100,100);
+        window2.addAction(Actions.sequence(Actions.alpha(0f), Actions.fadeIn(1f)));
+        stage.addActor(window2);
+
+
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0f, 0f, 0f, 1f);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
+        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
+        if(width <= 0 || height <= 0) return;
+
+        stage.getViewport().update(width, height);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
+}
